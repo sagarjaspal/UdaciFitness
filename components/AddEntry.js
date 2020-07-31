5ec2cd45fcb7eda7e1d2fae0ad0680434eb7a34f
sagarjaspal <sagar.jaspal.1509@gmail.com>
2020-07-31 21:39:25 +0530

    feat: add AddEntry component and render on root component

---
+++ components/AddEntry.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import { getMetricMetaInfo } from "../utils/helpers";
+
+class AddEntry extends Component {
+  state = {};
+  render() {
+    return <View>{getMetricMetaInfo("bike").getIcon()}</View>;
+  }
+}
+
+export default AddEntry;
